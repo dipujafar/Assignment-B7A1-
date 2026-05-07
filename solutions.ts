3894dcb36023233f9912196d5b8f00173f5a2911
dipujafar <dipujafarmr@gmail.com>
2026-05-07 22:24:51 +0600

    completed porblem -2

--- solutions.ts
+++ solutions.ts
@@ -1,7 +1,11 @@
-//problem 1 
+//problem 1
 const filterEvenNumbers = (numbers: number[]): number[] => {
   const evenNum = numbers.filter((num: number) => num % 2 === 0);
   return evenNum;
 };
 
-
+// problem 2
+const reverseString = (text: string): string => {
+  const reverseStr = text.split("").reverse().join("");
+  return reverseStr;
+};
